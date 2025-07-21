6c1b1747856270eb7e522925b8c6155db459d1a7
bluVshal <blu.vishal@gmail.com>
2025-07-21 23:55:41 +0400

    Added Bootstrap and Nav component in Components folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Nunavbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Nunavbar />
       </header>
     </div>
   );
---
+++ src/components/navbar/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Nunavbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg">
+                <a className="navbar-brand" href="http://google.com">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="http://google.com">Login</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="http://google.com/">Orders</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Nunavbar;
