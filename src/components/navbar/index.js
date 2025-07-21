69be167f1aee66f9591996793c2a225029e597a7
bluVshal <blu.vishal@gmail.com>
2025-07-22 00:48:41 +0400

    Modifications added

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -4,17 +4,17 @@ const Nunavbar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg">
-                <a className="navbar-brand" href="http://google.com">Navbar</a>
+                <a className="navbar-brand" href="http://google.com">NU|PIZZA</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item active">
                             <a className="nav-link" href="http://google.com">Login</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="http://google.com/">Orders</a>
+                            <a className="nav-link" href="http://google.com/">Cart</a>
                         </li>
 
                     </ul>
